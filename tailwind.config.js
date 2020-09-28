c7f1fab6d8bbaac8de70e26be711a855e06e6d3b
Shin'ichiro SUZUKI <shin@sforzando.co.jp>
2020-09-28 17:56:40 +0900

    :sparkles: Add purge options to tailswind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: [ "./app/**/*.html.erb", './app/javascript/**/*.js' ],
   theme: {
     extend: {},
   },
